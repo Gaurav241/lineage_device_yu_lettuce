d25553c7d29ac9ff92e38895afedbb9c6c18dce8
Gaurav241 <gaurav.govinda2420@gmail.com>
2019-11-07 17:27:07 +0530

    lettuce: Add qcom privapp permissions
    --> Source :- https://source.android.com/devices/tech/config/perms-whitelist#generating-whitelists
    --> Kang :- https://github.com/ArrowOS-Devices/android_device_yu_lettuce/commit/cae554eb0383612e9201cc6bb67d8ec2d4b04648

--- device.mk
+++ device.mk
@@ -111,6 +111,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Privapp Whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
+
 # Radio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml \
