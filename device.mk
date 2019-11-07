3b961b551d2134d4e8d007642973250033d62150
Gaurav241 <gaurav.govinda2420@gmail.com>
2019-11-07 17:08:59 +0530

    lettuce: Bring Back IMS

--- device.mk
+++ device.mk
@@ -121,6 +121,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshims_boringssl \
+    libshims_camera \
+    libshims_ims
+
 # Thermals
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
